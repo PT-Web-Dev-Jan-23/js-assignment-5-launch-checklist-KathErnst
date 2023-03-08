bfd819d411e9de21ec25fc62ec47eb25969b8e0d
Katherine Ernsting <katherineernsting@Katherines-MacBook-Air.local>
2023-03-07 22:51:44 -0600

    Created validateInput function and formSubmission function

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,52 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+    if (testInput === "") { 
+        return "Empty";
+        } else if (isNaN(testInput)) {
+            return "Not a number";
+        } else if (!isNaN(testInput)) {
+            return "Is a number";
+        }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    let form = document.getElementById("testForm");
+    form.addEventListener("submit", function (event) {
+        const pilotNameInput = document.querySelector("input[name=pilotName]");
+        const copilotNameInput = document.querySelector("input[name=copilotName]");
+        const fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+        const cargoMassInput = document.querySelector("input[name=cargoMass]");
+
+        const faultyItems = document.getElementById("faultyItems");
+        const pilotStatus = document.getElementById("pilotStatus");
+        const copilotStatus = document.getElementById("copilotStatus");
+        const fuelStatus = document.getElementById("fuelStatus");
+        const launchStatus = document.getElementById("launchStatus");
+        const cargoStatus = document.getElementById("cargoStatus");
+
+        if ((validateInput(pilot) === "Empty") || (validateInput(copilot) === "Empty") || 
+        (validateInput(fuelLevel) === "Empty") || (validateInput(cargoLevel) === "Empty")) {
+            alert("All fields are required!");
+            event.preventDefault();
+        } else if (  (validateInput(pilot) === "Is a number") || (validateInput(copilot) === "Is a number") || 
+        (validateInput(fuelLevel) === "Not a number") || (validateInput(cargoLevel) === "Not a number")  ) {
+            alert("Make sure to enter valid information for each field!");
+            event.preventDefault();
+        } else {
+            pilotStatus.innerHTML = `Pilot ${pilotNameInput} is ready for launch.`;
+            copilotStatus.innerHTML = `CoPilot ${copilotNameInput} is ready for launch.`;
+                if (fuelLevelInput < 10000) {
+                    faultyItems.style.visibility = "visible";
+                    fuelStatus.innerHTML = "Fuel level too low for launch.";
+                    launchStatus.innerHTML = "Shuttle not ready for launch";
+                    launchStatus.style.color = "red";
+                } else if (cargoMassInput > 10000) {
+                    faultyItems.style.visibility = "visible";
+                    cargoStatus.innerHTML = "Cargo mass too heavy for launch."
+            } //TEMPLATE LITERAL?? ^^^^^^^^^
+        }
+    })
 }
 
 async function myFetch() {
